18a6cb0e6735d35f74cf50f6eec44e831b5a4cee
Moises Rafael <rafael.plascencia@alumnos.udg.mx>
2020-09-20 19:30:19 -0500

    Se calcula el area de un cuadrado

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 int main(){
-    int p;
+    float lado;
     printf("hola mundo");
-
+    printf("Para calcular el área de un cuadro ingrese el tamaño de uno de los lados en cm:");
+    scanf("%f.2",&lado);
+    printf("%f.2 cm2",lado*lado);
     
     return 0;
 }
