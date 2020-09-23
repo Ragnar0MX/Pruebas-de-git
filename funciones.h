37669a8238e2f58951cbf0f084e70acddf7f5938
Moises Rafael <rafael.plascencia@alumnos.udg.mx>
2020-09-23 10:08:32 -0500

    Se completo la impresión y sumatoria de la función suma, ademas del calculo e impresión del aread de un cuadro

---
+++ funciones.h
@@ -0,0 +1,14 @@
+#ifndef FUNCIONES.H
+#define FUNCIONES.H
+#include <stdio.h>
+
+void areaC(int a){
+    printf("Area del cuadrado es: %d",a*a);
+}
+
+void suma(int a, int b){
+    printf("%d + %d = %d",a+b);
+}
+
+
+#endif
